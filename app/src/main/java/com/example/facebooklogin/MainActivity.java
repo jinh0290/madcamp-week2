82c98242a4401b06990fabbc7d87f0be398d88e1
jinh0290 <wlsgur0810@gmail.com>
2020-07-19 23:40:07 +0900

    init

---
+++ app/src/main/java/com/example/facebooklogin/MainActivity.java
@@ -0,0 +1,248 @@
+package com.example.facebooklogin;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+import android.widget.TextView;
+
+import com.bumptech.glide.request.RequestOptions;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    // https://www.youtube.com/watch?v=q0jAFmB-wkU&t=433s 참고
+    private LoginButton loginButton;
+    private TextView txtName;
+    private CallbackManager callbackManager;
+
+
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL = "http://192.249.19.242:8380";
+
+    public static final int sub = 1001; /*다른 액티비티를 띄우기 위한 요청코드(상수)*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loginButton = findViewById(R.id.login_button);
+        txtName = findViewById(R.id.tv_name);
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.setReadPermissions(Arrays.asList("email", "public_profile"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // change design of continue button
+//                EditText editText = (EditText) findViewById(R.id.editText);
+//                String message = editText.getText().toString();
+//                intent.putExtra(EXTRA_MESSAGE, message);
+//                startActivity(intent);
+
+            }
+            @Override
+            public void onCancel() {
+            }
+            @Override
+            public void onError(FacebookException error) {
+            }
+        });
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleLoginDialog();
+            }
+        });
+
+        findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleSignupDialog();
+            }
+        });
+
+    }
+    // activity A와 B가 있을 때, A에서 B로 갔다가 다시 A로 넘어올 때 사용하는 메소드
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode , resultCode , data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    // 로그인 콜백 대신 AccessTokenTracker 사용, 앱에서 계속 추적 가능
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken == null) {
+                txtName.setText("");
+//                txtEmail.setText("");
+//                circleImageView.setImageResource(0);
+                Toast.makeText(MainActivity.this, "User Logged out", Toast.LENGTH_LONG).show();
+            } else loaduserProfile(currentAccessToken);
+        }
+    };
+    private void loaduserProfile(AccessToken newAccessToken) {
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                String first_name = null;
+                String last_name = null;
+                try {
+                    first_name = object.getString("first_name");
+                    last_name = object.getString("last_name");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                txtName.setText(first_name+" "+last_name);
+                RequestOptions requestOptions = new RequestOptions();
+                requestOptions.dontAnimate();
+            }
+        });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "first_name, last_name, email, id");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+    private void handleLoginDialog() {
+
+        View view = getLayoutInflater().inflate(R.layout.login_dialog, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setView(view).show();
+
+        Button loginBtn = view.findViewById(R.id.login);
+        final EditText emailEdit = view.findViewById(R.id.emailEdit);
+        final EditText passwordEdit = view.findViewById(R.id.passwordEdit);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                HashMap<String, String> map = new HashMap<>();
+
+                map.put("email", emailEdit.getText().toString());
+                map.put("password", passwordEdit.getText().toString());
+
+                Call<LoginResult2> call = retrofitInterface.executeLogin(map);
+
+                call.enqueue(new Callback<LoginResult2>() {
+                    @Override
+                    public void onResponse(Call<LoginResult2> call, Response<LoginResult2> response) {
+
+                        if (response.code() == 200) {
+
+                            LoginResult2 result = response.body();
+
+                            AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                            assert result != null;
+                            builder1.setTitle(result.getName());
+                            builder1.setMessage(result.getEmail());
+
+                            builder1.show();
+
+                        } else if (response.code() == 404) {
+                            Toast.makeText(MainActivity.this, "Wrong Credentials",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginResult2> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, t.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        });
+
+    }
+
+    private void handleSignupDialog() {
+
+        View view = getLayoutInflater().inflate(R.layout.signup_dialog, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(view).show();
+
+        Button signupBtn = view.findViewById(R.id.signup);
+        final EditText nameEdit = view.findViewById(R.id.nameEdit);
+        final EditText emailEdit = view.findViewById(R.id.emailEdit);
+        final EditText passwordEdit = view.findViewById(R.id.passwordEdit);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                HashMap<String, String> map = new HashMap<>();
+
+                map.put("name", nameEdit.getText().toString());
+                map.put("email", emailEdit.getText().toString());
+                map.put("password", passwordEdit.getText().toString());
+
+                Call<Void> call = retrofitInterface.executeSignup(map);
+
+                call.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+
+                        if (response.code() == 200) {
+                            Toast.makeText(MainActivity.this,
+                                    "Signed up successfully", Toast.LENGTH_LONG).show();
+                        } else if (response.code() == 400) {
+                            Toast.makeText(MainActivity.this,
+                                    "Already registered", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, t.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        });
+
+    }
+
+}
+
---
+++ app/src/main/java/com/example/facebooklogin/RetrofitInterface.java
@@ -0,0 +1,17 @@
+package com.example.facebooklogin;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface RetrofitInterface {
+
+    @POST("/login")
+    Call<LoginResult2> executeLogin(@Body HashMap<String, String> map);
+
+    @POST("/signup")
+    Call<Void> executeSignup(@Body HashMap<String, String> map);
+
+}

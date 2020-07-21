19ded200f2f80068feb04efa8db5aac66d8dc066
jinh0290 <wlsgur0810@gmail.com>
2020-07-22 01:00:26 +0900

    0

---
+++ app/src/main/java/com/example/facebooklogin/tap3Activity/Tap3Activity.java
@@ -0,0 +1,14 @@
+package com.example.facebooklogin.tap3Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tap3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tap3);
+    }
+}
